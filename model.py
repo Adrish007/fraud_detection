61082243c0ba4fdbd070cfaca208156d807e4884
Adrish007 <adrishray123@gmail.com>
2023-11-19 00:43:11 +0530

    added 3 new model code

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("Adding all M related code here")
+
+def model1():
+    print("Building Model 1")
+
+def model2():
+    print("Building Model 2")
+
+def model3():
+    print("Building Model 3")
